737a104aafa1ae901fccc729e13cc7957897c1b3
Usagi no Niku <chenbohan911@163.com>
2024-05-14 10:20:43 +0800

    add mouse and keyboard event

--- Minesweeper/Minesweeper.cpp
+++ Minesweeper/Minesweeper.cpp
@@ -1,18 +1,15 @@
 ﻿#include <graphics.h>
 #include <conio.h>
 #include <iostream>
-#include <thread>
-#include <mutex>
 
 class Slot {
 	public:
+		int type;
 		Slot(int type, float posX, float posY, float pixel);
-		void Draw();
-		void AttachType(int type);
-		int GetType() const;
+		void Draw(bool visible);
+		void OnMouseClick();
 
 	private:
-		int type;
 		float posX;
 		float posY;
 		float pixel;
@@ -25,21 +22,20 @@ Slot::Slot(int type, float posX, float posY, float pixel) {
 	this->pixel = pixel;
 }
 
-void Slot::Draw() {
+void Slot::Draw(bool visible) {
 	IMAGE image = NULL;
 	wchar_t filePath[100];
 
-	swprintf_s(filePath, L"Images\\%d.jpg", this->type);
+	// if it is not visible, we only show walls to the user
+	int target = visible ? this->type : 0;
+
+	swprintf_s(filePath, L"Images\\%d.jpg", target);
 	loadimage(&image, filePath, int(pixel), int(pixel), true);
 	putimage(int(posX), int(posY), &image, SRCCOPY);
 }
 
-void Slot::AttachType(int type) {
-	this->type = type;
-}
-
-int Slot::GetType() const {
-	return this->type;
+void Slot::OnMouseClick() {
+	Draw(true);
 }
 
 class MineSweeper {
@@ -54,13 +50,15 @@ class MineSweeper {
 		int windowSize;
 		int elementSize;
 		bool ValidateBomb(int index);
+		void DrawSlots(bool visible);
 		void StartEventLoop();
+		void OnSlotClicked(Slot slot, int x, int y);
 };
 
 MineSweeper::MineSweeper(int windowSize, int dimension) {
 	this->dimension = dimension;
 	this->windowSize = windowSize;
-	this->bombNum = 5;
+	this->bombNum = 16;
 	this->elementSize = windowSize / dimension;
 	this->slots = (Slot*) malloc(sizeof(Slot) * dimension * dimension);
 }
@@ -79,13 +77,13 @@ void MineSweeper::CreateGame() {
 	// add bombs to specific slots
 	for (int i = 0; i < bombNum; i++) {
 		int slotIndex = rand() % (dimension * dimension);
-		slots[slotIndex].AttachType(-1);
+		slots[slotIndex].type = -1;
 	}
 
 	// calculate bomb nums
 	for (int i = 0; i < dimension * dimension; i++) {
 		int surrounds = 0;
-		if (slots[i].GetType() == -1) continue;
+		if (slots[i].type == -1) continue;
 		if (ValidateBomb(i - 1)) surrounds += 1;
 		if (ValidateBomb(i + 1)) surrounds += 1;
 		if (ValidateBomb(i - dimension)) surrounds += 1;
@@ -94,29 +92,48 @@ void MineSweeper::CreateGame() {
 		if (ValidateBomb(i + dimension)) surrounds += 1;
 		if (ValidateBomb(i + dimension - 1)) surrounds += 1;
 		if (ValidateBomb(i + dimension + 1)) surrounds += 1;
-		slots[i].AttachType(surrounds);
+		slots[i].type = surrounds;
 	}
 
-	// draw all the slots
-	for (int i = 0; i < dimension * dimension; i++) {
-		slots[i].Draw();
-	}
+	// draw all the slots with no visiblity
+	DrawSlots(false);
 
 	StartEventLoop();
 }
 
-bool MineSweeper::ValidateBomb(int index)
-{
+bool MineSweeper::ValidateBomb(int index) {
 	if (index >= 0 && index < dimension * dimension) {
-		return slots[index].GetType() == -1;
+		return slots[index].type == -1;
 	}
 	return false; // that index is not exist
 }
 
-void MineSweeper::StartEventLoop () {
-	while (true) {
-		int key = _getch();
+void MineSweeper::DrawSlots(bool visible) {
+	for (int i = 0; i < dimension * dimension; i++) {
+		slots[i].Draw(visible);
 	}
+}
+
+void MineSweeper::StartEventLoop() {
+	while (true) {
+		ExMessage msg = getmessage(EX_MOUSE | EX_KEY);
+		if (msg.message == WM_LBUTTONDOWN) {
+			int x = msg.x / elementSize;
+			int y = msg.y / elementSize;
+			OnSlotClicked(slots[x * dimension + y], y, x);
+		}
+		if (msg.message = WM_KEYDOWN && msg.vkcode == 0x56) {
+			DrawSlots(true); // V Key: Show slots
+		}
+		if (msg.message = WM_KEYDOWN && msg.vkcode == 0x48) {
+			DrawSlots(false); // H Key: Hide slots
+		}
+	}
+}
+
+void MineSweeper::OnSlotClicked(Slot slot, int x, int y) {
+	slot.OnMouseClick();
+	// TODO: extend related slots, and fire the bombs.
 }
 
 int main()
